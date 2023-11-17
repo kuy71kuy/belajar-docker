c82a232aae35a81f8acaed98758ec0a46ca06e74
kuy71kuy <kuy71kuy@gmail.com>
2023-11-17 22:29:19 +0700

    update

--- constants/constants.go
+++ constants/constants.go
@@ -1,15 +1,9 @@
 package constants
 
 import (
-	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 func LoadEnv(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	return os.Getenv(key)
 }
